1093ee528c364fa08cc5eeada0df9e8aceec7e5e
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-06-24 15:50:43 +0200

    added-js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+Mail
+Crea (inventa) una lista di inidirizzi mail autorizzati.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilisci il vincitore, in base a chi fa il punteggio più alto e stampa  in console il risultato (ossia se ha vinto il computer o il player)
+*/
+
+const mailAddress = ["fabio.folino@gmail.com" , "matteo.galletti@gmail.com" , "ilaria.trecca@gmail.com" , "francesca.maurelli@gmail.com"];
+const access = prompt('inserisci la tua mail');
+
+console.table(mailAddress);
+
+if (access === mailAddress){
+    console.log('Benvenuto');
+}
+else if (access !== mailAddress){
+    console.log('Spiacenti');
+}
